0f0d215ddfdaeab188258d1d9433442e19619604
Michael R.F <mrflowers22@uwalumni.com>
2018-01-22 15:37:49 -0700

    background colors

---
+++ Flashlight project 1/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Flashlight project 1
+//
+//  Created by Michael Flowers on 1/22/18.
+//  Copyright © 2018 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var isOn: Bool = true
+    @IBOutlet weak var onlyButton: UIButton!
+    @IBAction func onoffButtonPressed(_ sender: Any) {
+        isOn = !isOn
+        
+        if isOn {
+            view.backgroundColor = UIColor.blue
+            onlyButton.backgroundColor = .black
+            onlyButton.titleLabel?.backgroundColor = .white
+            
+        } else {
+            view.backgroundColor = UIColor.orange
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
